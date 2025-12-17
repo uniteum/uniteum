666cab7fc4995c9b8134402c2e7bfca067335d82
Paul Reinholdtsen <paul@bitsy.services>
2025-12-17 11:00:59 -0800

    Deploy Uniteum 0.2 to Sepolia

--- script/Unit.s.sol
+++ script/Unit.s.sol
@@ -8,12 +8,12 @@ import {Unit, IERC20} from "../src/Unit.sol";
 /// @notice Deploy the BridgeFactory contract.
 /// @dev Usage: forge script script/Unit.s.sol -f $chain --private-key $tx_key --broadcast --verify --delay 10 --retries 10
 contract UnitCreate2 is Script {
-    address constant UPSTREAM_ONE = 0xC833f0B7cd7FC479DbbF6581EB4eEFc396Cf39E4;
+    address constant UPSTREAM = 0xfe5dEC33a41a20e3A6F4b524713597fa8779d8B1;
 
     function run() public {
         vm.startBroadcast();
 
-        Unit unit = new Unit{salt: 0x0}(IERC20(UPSTREAM_ONE));
+        Unit unit = new Unit{salt: 0x0}(IERC20(UPSTREAM));
         console2.log("Deployed Unit at:", address(unit));
 
         vm.stopBroadcast();
--- script/verifyUnit.sh
+++ script/verifyUnit.sh
@@ -1,3 +1,3 @@
 contract=$(jq -r '.transactions[0].contractAddress' broadcast/Unit.s.sol/$chain/dry-run/run-latest.json)
 args=$(cast abi-encode "constructor(address)" $(jq -r '.transactions[].arguments[0]' broadcast/Unit.s.sol/$chain/dry-run/run-latest.json))
-forge verify-contract $contract Unit --chain $chain --verifier etherscan --show-standard-json-input > io/$chain/Unit.json
+forge verify-contract $contract Unit --chain $chain --verifier etherscan --show-standard-json-input > io/Unit.json
