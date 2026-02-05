f338b21aa3d8217c9a3a5084d462b267569d92f3
Paul Reinholdtsen <paul@bitsy.services>
2026-02-04 21:00:07 -0800

    Delete kiosk, mintlet, and uniteum related files

--- script/verifyDiscountKiosk.sh
+++
@@ -1,2 +0,0 @@
-contract=$(jq -r '.transactions[0].contractAddress' broadcast/DiscountKiosk.s.sol/$chain/dry-run/run-latest.json)
-forge verify-contract $contract DiscountKiosk --chain $chain --verifier etherscan --show-standard-json-input > io/$chain/DiscountKiosk.json
--- script/verifyUniteum.sh
+++
@@ -1,2 +0,0 @@
-contract=$(jq -r '.transactions[0].contractAddress' broadcast/Uniteum.s.sol/$chain/dry-run/run-latest.json)
-forge verify-contract $contract Uniteum --chain $chain --verifier etherscan --show-standard-json-input > io/$chain/Uniteum.json
