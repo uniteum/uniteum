fa120ceb3ecff6889bcaa17e56d5a86cedfd39c1
Paul Reinholdtsen <paul@bitsy.services>
2026-02-04 21:20:59 -0800

    Uniteum-0.7

--- script/Unit.s.sol
+++ script/Unit.s.sol
@@ -8,12 +8,13 @@ import {Unit, IERC20} from "../src/Unit.sol";
 /// @notice Deploy the BridgeFactory contract.
 /// @dev Usage: forge script script/Unit.s.sol -f $chain --private-key $tx_key --broadcast --verify --delay 10 --retries 10
 contract UnitCreate2 is Script {
-    address constant UPSTREAM = 0x8af6774Bec3b698a6dC873b117c4415367dEfea6;
-
     function run() public {
+        address hub = vm.envAddress("HUB_SOLID");
+        console2.log("HUB_SOLID at:", hub);
+
         vm.startBroadcast();
 
-        Unit unit = new Unit{salt: 0x0}(IERC20(UPSTREAM));
+        Unit unit = new Unit{salt: 0x0}(IERC20(hub));
         console2.log("Deployed Unit at:", address(unit));
 
         vm.stopBroadcast();
